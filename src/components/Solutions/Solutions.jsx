fa16c290db48754eb979e09f8a90bbd0da343ba2
Islomjonsdev <islomjon@immigrateable.com>
2024-12-11 23:58:28 +0500

    done some places

--- src/components/Solutions/Solutions.jsx
+++ src/components/Solutions/Solutions.jsx
@@ -1,10 +1,22 @@
-import React from 'react';
-import style from "./Solutions.module.scss"
+import React from "react";
+import solutionsImage from "../../assets/images/solutions.png";
+import style from "./Solutions.module.scss";
+import SolutionsRight from "./SolutionsRight/SolutionsRight";
 
 const Solutions = () => {
   return (
-    <section className={style.solutions}>Solutions</section>
-  )
-}
+    <section className={style.solutions}>
+      <div className={style.container}>
+        <div className={style.solutions_container}>
+          <div className={style.solutions_left}>
+            <div className={style.solutions_left_color}></div>
+            <img src={solutionsImage} alt="" />
+          </div>
+          <SolutionsRight />
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Solutions
+export default Solutions;
---
+++ src/components/Solutions/SolutionsRight/SolutionsRight.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Text from "../../ui/Text/Text";
+import style from "./SolutionsRight.module.scss";
+
+const SolutionsRight = () => {
+  return (
+    <div className={style.solution_right}>
+      <div className={style.solution_right_top}>
+        <Text title={"Why choose us"} />
+        <h4>Proven Expertise in Complex IT Solutions</h4>
+        <p>
+          Assertively streamline interactive interfaces after value-added
+          infrastructures. Authoritatively fabricate fully tested methodologies
+          before. Holistic deliver end-to-end architectures rather than
+          economically sound benefits. Progressively simplify end-to-end
+          paradigms. Assertively streamline interactive interfaces after
+          value-added infrastructure.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default SolutionsRight;
